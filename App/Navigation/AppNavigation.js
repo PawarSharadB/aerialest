0fe850d7f6a2ff3bc334401edfa9d4aa031d156d
sharad-epam <sharad_pawar@epam.com>
2020-02-17 17:42:16 +0530

    Added order button

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -6,6 +6,7 @@ import {
   createSwitchNavigator
 } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
+import Button from '../Components/Button'
 import HomePage from '../Containers/HomeScreen'
 import AboutUs from '../Containers/AboutUs'
 import BillingInfo from '../Containers/BillingInfo'
@@ -13,19 +14,34 @@ import ContactUs from '../Containers/ContactUs'
 import Pricing from '../Containers/Pricing'
 import Profile from '../Containers/Profile'
 import LaunchScreen from '../Containers/LaunchScreen'
-
 import Login from '../Containers/Login'
 import ForgotPassword from '../Containers/ForgotPassword'
-
 import { SelectAddress } from '../Containers/SelectAddress'
 import Register from '../Components/Register'
 import NavBar from '../Components/NavBar'
 import NavBackButton from '../Components/NavBackButton'
 import SignoutScreen from '../Containers/SignoutScreen'
-
 const getNavigationOptions = (navigation, title) => ({
   title,
   headerLeft: <NavBar navigationProps={navigation} />,
+  headerRight: () => (
+    <Button
+      style={{
+        minWidth: 50,
+        backgroundColor: 'white',
+        height: 30,
+        justifyContent: 'center',
+        marginRight: 10
+      }}
+      textStyle={{
+        alignSelf: 'center',
+        paddingVertical: 0,
+        color: 'black'
+      }}
+      onPress={() => navigation.navigate('OrdersStack')}
+      text="Order"
+    />
+  ),
   headerStyle: {
     backgroundColor: '#0485B2'
   },
@@ -39,7 +55,6 @@ const getNavigationOptionsWithBackButton = (navigation, title) => ({
   },
   headerTintColor: '#fff'
 })
-
 const HomeStack = createStackNavigator({
   HomeScreen: {
     screen: HomePage,
@@ -89,7 +104,6 @@ const BillingInfoStack = createStackNavigator({
       getNavigationOptions(navigation, 'BillingInfo')
   }
 })
-
 const RegisterStack = createStackNavigator({
   RegisterInfo: {
     screen: Register,
@@ -109,112 +123,119 @@ const LoginStack = createStackNavigator({
       getNavigationOptionsWithBackButton(navigation, 'Reset Password')
   }
 })
-
-const authNavigator = createDrawerNavigator({
-  Home: {
-    screen: HomeStack,
-    navigationOptions: {
-      drawerLabel: 'Home',
-      drawerIcon: <Icon name="home" size={20} />
+const authNavigator = createDrawerNavigator(
+  {
+    Home: {
+      screen: HomeStack,
+      navigationOptions: {
+        drawerLabel: 'Home',
+        drawerIcon: <Icon name="home" size={20} />
+      }
+    },
+    AboutUs: {
+      screen: AboutUsStack,
+      navigationOptions: {
+        drawerLabel: 'About Us',
+        drawerIcon: <Icon name="flag" size={20} />
+      }
+    },
+    Pricing: {
+      screen: PricingStack,
+      navigationOptions: {
+        drawerLabel: 'Pricing',
+        drawerIcon: <Icon name="tags" size={20} />
+      }
+    },
+    ContactUs: {
+      screen: ContactUsStack,
+      navigationOptions: {
+        drawerLabel: 'Contact Us',
+        drawerIcon: <Icon name="envelope" size={20} />
+      }
+    },
+    OrdersStack: {
+      screen: OrdersStack,
+      navigationOptions: {
+        drawerLabel: 'Orders',
+        drawerIcon: <Icon name="thumbs-o-up" size={20} />
+      }
+    },
+    Profile: {
+      screen: ProfileStack,
+      navigationOptions: {
+        drawerLabel: 'Profile',
+        drawerIcon: <Icon name="gear" size={20} />
+      }
+    },
+    BilligInfo: {
+      screen: BillingInfoStack,
+      navigationOptions: {
+        drawerLabel: 'Billing Info',
+        drawerIcon: <Icon name="gear" size={20} />
+      }
+    },
+    Signout: {
+      screen: SignoutScreen,
+      navigationOptions: {
+        drawerLabel: 'Sign out',
+        drawerIcon: <Icon name="power-off" size={20} />
+      }
     }
   },
-  AboutUs: {
-    screen: AboutUsStack,
-    navigationOptions: {
-      drawerLabel: 'About Us',
-      drawerIcon: <Icon name="flag" size={20} />
-    }
-  },
-  Pricing: {
-    screen: PricingStack,
-    navigationOptions: {
-      drawerLabel: 'Pricing',
-      drawerIcon: <Icon name="tags" size={20} />
-    }
-  },
-  ContactUs: {
-    screen: ContactUsStack,
-    navigationOptions: {
-      drawerLabel: 'Contact Us',
-      drawerIcon: <Icon name="envelope" size={20} />
-    }
-  },
-  OrdersStack: {
-    screen: OrdersStack,
-    navigationOptions: {
-      drawerLabel: 'Orders',
-      drawerIcon: <Icon name="thumbs-o-up" size={20} />
-    }
-  },
-  Profile: {
-    screen: ProfileStack,
-    navigationOptions: {
-      drawerLabel: 'Profile',
-      drawerIcon: <Icon name="gear" size={20} />
-    }
-  },
-  BilligInfo: {
-    screen: BillingInfoStack,
-    navigationOptions: {
-      drawerLabel: 'Billing Info',
-      drawerIcon: <Icon name="gear" size={20} />
-    }
-  },
-  Signout: {
-    screen: SignoutScreen,
-    navigationOptions: {
-      drawerLabel: 'Sign out',
-      drawerIcon: <Icon name="power-off" size={20} />
-    }
+  {
+    overlayColor: 'rgba(0, 0, 0, 0.8)'
   }
-})
-const nonAuthNavigator = createDrawerNavigator({
-  Home: {
-    screen: HomeStack,
-    navigationOptions: {
-      drawerLabel: 'Home',
-      drawerIcon: <Icon name="home" size={20} />
+)
+const nonAuthNavigator = createDrawerNavigator(
+  {
+    Home: {
+      screen: HomeStack,
+      navigationOptions: {
+        drawerLabel: 'Home',
+        drawerIcon: <Icon name="home" size={20} />
+      }
+    },
+    AboutUs: {
+      screen: AboutUsStack,
+      navigationOptions: {
+        drawerLabel: 'About Us',
+        drawerIcon: <Icon name="flag" size={20} />
+      }
+    },
+    Pricing: {
+      screen: PricingStack,
+      navigationOptions: {
+        drawerLabel: 'Pricing',
+        drawerIcon: <Icon name="tags" size={20} />
+      }
+    },
+    ContactUs: {
+      screen: ContactUsStack,
+      navigationOptions: {
+        drawerLabel: 'Contact Us',
+        drawerIcon: <Icon name="envelope" size={20} />
+      }
+    },
+    Login: {
+      screen: LoginStack,
+      navigationOptions: {
+        drawerLabel: 'Sign In',
+        drawerIcon: <Icon name="paper-plane-o" size={20} />
+      }
+    },
+    Register: {
+      screen: RegisterStack,
+      navigationOptions: {
+        drawerLabel: 'Register',
+        drawerIcon: <Icon name="user-plus" size={20} />
+      }
     }
   },
-  AboutUs: {
-    screen: AboutUsStack,
-    navigationOptions: {
-      drawerLabel: 'About Us',
-      drawerIcon: <Icon name="flag" size={20} />
-    }
-  },
-  Pricing: {
-    screen: PricingStack,
-    navigationOptions: {
-      drawerLabel: 'Pricing',
-      drawerIcon: <Icon name="tags" size={20} />
-    }
-  },
-  ContactUs: {
-    screen: ContactUsStack,
-    navigationOptions: {
-      drawerLabel: 'Contact Us',
-      drawerIcon: <Icon name="envelope" size={20} />
-    }
-  },
-  Login: {
-    screen: LoginStack,
-    navigationOptions: {
-      drawerLabel: 'Sign In',
-      drawerIcon: <Icon name="paper-plane-o" size={20} />
-    }
-  },
-  Register: {
-    screen: RegisterStack,
-    navigationOptions: {
-      drawerLabel: 'Register',
-      drawerIcon: <Icon name="user-plus" size={20} />
-    }
+  {
+    overlayColor: 'rgba(0, 0, 0, 0.8)'
   }
-})
-
+)
 //export default createAppContainer(drawerNavigator)
-
 export default createAppContainer(
   createSwitchNavigator(
     {
